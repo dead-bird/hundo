fc069e6f1f101dc4dded1ce477a11580b5e91921
DaveP <dave@wearecrystal.uk>
2018-08-01 17:22:50 +0100

    Bot scaffold

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: '.env' });
 
-// server invite link:
+// Server invite link:
 // https://discordapp.com/oauth2/authorize?client_id=474240197329158165&scope=bot&permissions=34816
 
 module.exports = {
@@ -12,6 +12,6 @@ module.exports = {
   // MANAGE_MESSAGES - 	0x00002000
   deleteAfterReply: {
     enabled: false,
-    time: 10000, // In milliseconds
-  },
+    time: 10000 // In milliseconds
+  }
 };
