4ddfec785741fc146b551cce78f7d2e2b9ed56af
DaveP <dave@wearecrystal.uk>
2018-08-01 17:22:04 +0100

    Bot scaffold

---
+++ config.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: '.env' });
+
+// server invite link:
+// https://discordapp.com/oauth2/authorize?client_id=474240197329158165&scope=bot&permissions=34816
+
+module.exports = {
+  name: 'Hundo',
+  prefix: '-hundo',
+  token: process.env.TOKEN,
+
+  // ATTENTION! In order for this to work, you need to give your bot the following permission:
+  // MANAGE_MESSAGES - 	0x00002000
+  deleteAfterReply: {
+    enabled: false,
+    time: 10000, // In milliseconds
+  },
+};
