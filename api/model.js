4cd79b31aeb11667baa39fda72b4934a0e734354
DaveP <dave@wearecrystal.uk>
2018-08-27 23:25:52 +0100

    tidy

--- api/model.js
+++ api/model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-let schema = new mongoose.Schema({
+const schema = new mongoose.Schema({
   words: { type: String, required: true },
   date: { type: Date, default: Date.now },
 });
